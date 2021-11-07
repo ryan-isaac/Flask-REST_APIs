2e41499e15e42d67f6367ca2226450534f6dc907
Ryan Isaac <60246719+saudtech@users.noreply.github.com>
2021-11-06 20:23:54 -0400

    Add files via upload

---
+++ Simple Flask-SQLite3/item.py
@@ -0,0 +1,146 @@
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+import sqlite3
+
+#CRUPT Create Read Update Delete
+
+#every resource has to be a class
+class Item(Resource):
+#--------------------------------------------------------#
+# classmethods that can be accessed by all functions
+#--------------------------------------------------------#
+# this parser can be used for any method inside the class
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required = True,
+        help="This field cannot be left blank!"
+    )
+
+    # create a class method to use it in different requests (GET, POST) without duplicating it
+    # class method can be called by self or Item that is the class name
+    @classmethod
+    def find_by_name(cls,name):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM items WHERE name= ?"
+        result = cursor.execute(query, (name,))
+        row = result.fetchone()
+        connection.close()
+
+        if row != None:
+            return {"item": {'name': row[0], 'price': row[1]}}
+
+    # PUT can't update an existing item in sqlite3, therefore,
+    # the creation of an item from POST method will be used as a classmethod
+    # to be accessed in both PUT and POST requests
+        # insert an item to the database
+    @classmethod
+    def insert(cls, item):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        # insert into the database
+        query = "INSERT INTO items VALUES (?,?)"
+        cursor.execute(query, (item['name'],item['price']))
+
+        connection.commit()
+        connection.close()
+
+
+    @classmethod
+    def update(cls, item): #item parameter is a dictionaty of item and price
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        # update a record in the database
+        query = "UPDATE items SET price=? WHERE name =?"
+        cursor.execute(query, (item['price'],item['name']))
+
+        connection.commit()
+        connection.close()
+#--------------------------------------------------------#
+# class defined functions
+#--------------------------------------------------------#
+
+    jwt_required()
+    def get(self,name):
+        item = self.find_by_name(name)
+        try:
+            if item:
+                return item
+            return {'message': 'Item not found'}, 404
+        except:
+            return {'message': 'An error occured while fetching the data'}, 500
+
+    @jwt_required()
+    def post(self,name):
+        # check that the item is not in the database
+        if Item.find_by_name(name):
+            return {'message': "An item with name '{}' already exists.".format(name)}, 400 #bad request, client shoudl know item exists so they get bad request for trying to post a name that already exists
+        # parse the data looking for price
+        data = Item.parser.parse_args()
+
+        item = {'name': name, 'price': data['price']}
+
+        try:
+            self.insert(item)
+
+        except:
+            return{"message": "An error occured while inserting the item"}, 500 # 500 is for internal server error
+
+        return item, 201
+
+    @jwt_required()
+    def delete(self, name):
+        if Item.find_by_name != None:
+            connection = sqlite3.connect('data.db')
+            cursor = connection.cursor()
+            # delete row where name = item name from the table
+            query = "DELETE FROM items WHERE name = ?"
+            cursor.execute(query, (name,))
+            connection.commit()
+            connection.close()
+
+            return {'message': 'Item deleted'}
+        return {"message": "Item doesn't exist or an error occured while deleting"}
+
+    @jwt_required()
+    def put(self, name):
+        data = Item.parser.parse_args()
+        item = self.find_by_name(name)
+        updated_item = {'name': name, 'price': data['price']}
+
+        if item == None:
+            try:
+                Item.insert(updated_item)
+                return{"message":f"Item has been created {updated_item}"}
+            except:
+                return{"message": "An error occured while inserting the item."}, 500
+        elif item != None:
+            try:
+                Item.update(updated_item)
+                return{"message":f"New item has been updated {updated_item}"}
+            except:
+                return{"message": "An error occured while updating the item."}, 500
+        else:
+            return {"message": "Error Unknown"}
+        return updated_item
+
+
+class ItemList(Resource):
+    @jwt_required()
+    def get(self):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        # select all items
+        query = "SELECT * FROM items"
+        result = cursor.execute(query)
+        items = [] # create a list for all the items to be stored temporarily
+        for row in result:
+            items.append({'name': row[0], 'price': row[1]})
+            
+        connection.close()
+
+        return {'items': items}
---
+++ Simple Flask-SQLite3/user.py
@@ -0,0 +1,106 @@
+import sqlite3
+from flask_restful import Resource, reqparse
+
+# user object instead of dictionaries
+class User:
+    def __init__(self, _id, username, password):
+        self.id = _id
+        self.username = username
+        self.password = password
+
+    # since we will use the class name User in "if row: user = User(row[0]...)"
+    # we should make it a class method to use the current class instead of hardcoding the class name if it will be changed in the future
+    @classmethod
+    # retrieve user object from the database by interacting with sqlite3
+    def find_by_username(cls, username):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        # search the table for a given username
+        query = "SELECT * FROM users WHERE username=?" # select only the rows where the username matches the parameter
+        # get the result of the query
+        result = cursor.execute(query, (username,)) # username must be in a tuple so we leave a comma
+        # get the first row from the result set if more than one
+        row = result.fetchone()
+        # if it wasn't None, if there is a found result
+        if row:
+            # create a user object with the data from that row for(row[0], row[1], row[2]) the 3 columns is, username, password
+            # instead of specifying the number of rows, we can use positional arguments (*args)
+            # change (row[0], row[1], row[2]) to (*row)
+            user = cls(*row)
+        else:
+            # return None if not found
+            user = None
+
+        connection.close()
+
+        #return the user or None id not found
+        return user
+
+    # since we will use the class name User in "if row: user = User(row[0]...)"
+    # we should make it a class method to use the current class instead of hardcoding the class name if it will be changed in the future
+    @classmethod
+    # retrieve user object from the database by interacting with sqlite3
+    def find_by_id(cls, id):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        # search the table for a given id
+        query = "SELECT * FROM users WHERE id=?" # select only the rows where the username matches the parameter
+        # get the result of the query
+        result = cursor.execute(query, (id,)) # id must be in a tuple so we leave a comma
+        # get the first row from the result set if more than one
+        row = result.fetchone()
+        # if it wasn't None, if there is a found result
+        if row:
+            #create a user object with the data from that row for(row[0], row[1], row[2]) the 3 columns is, username, password
+            # instead of specifying the number of rows, we can use positional arguments (*args)
+            # change (row[0], row[1], row[2]) to (*row)
+            id = cls(*row)
+        else:
+            # return None if not found
+            id = None
+
+        connection.close()
+
+        #return the user or None if not found
+        return id
+
+class UserRegister(Resource):
+# create a parser that looks for username
+    parser = reqparse.RequestParser()
+    parser.add_argument('username',
+        type= str,
+        required=True,
+        help="This field cannot be blank."
+    )
+# create a parser that looks for password
+    parser.add_argument('password',
+        type= str,
+        required=True,
+        help="This field cannot be blank."
+    )
+
+    def post(self):
+        # parse the argument that expects a username and a password
+        data = UserRegister.parser.parse_args()
+
+        # make sure not to register a duplicate username
+        # find_by_username returns the username or None if the user doesn't exist
+        # "if User.find_by_username" returns True if user exists, None if it doesn't
+        if User.find_by_username(data['username']):
+            return{"messager": "A user with that username already exists"}, 400
+        #connect to the database and create a cursor
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        # insert the values into the table with null as the id
+        query = "INSERT INTO users VALUES (NULL, ?, ?)" # null for the id as it auto increment
+        cursor.execute(query, (data['username'], data['password']))# username, pass has to be a tuple
+
+        #save the changes
+        connection.commit()
+        connection.close()
+
+        # response 201 for created
+        return {"message": "User created successfully."}, 201
